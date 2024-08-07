85c12df4a78adbde3aba3cd9e83170877697cf3f
lorenzo7377 <lorenzodavinci@hotmail.com>
2024-08-07 03:12:39 -0300

    Update model.py

--- model.py
+++ model.py
@@ -10,34 +10,30 @@ class StaticAgent(Agent):
         self.x = x
         self.y = y
         self.state = 'green'
-        self.red_steps = 0    # Contador de passos em que o agente está vermelho
+        self.red_steps = 0    
 
     def step(self):
         if self.state == 'red':
             self.red_steps += 1
-            # Se o agente estiver vermelho por mais de dois passos, muda para cinza
             if self.red_steps > 20:
                 self.state = 'gray'
         if self.state == 'green':
             neighbors = self.model.grid.get_neighbors((self.x, self.y), moore=True, include_center=False)
             red_neighbors = sum(1 for neighbor in neighbors if neighbor.state == 'red')
             
-            # Ajusta a probabilidade de mudar para vermelho
-            prob = 0.003 * (2 ** red_neighbors)  # Dobra a chance para cada vizinho vermelho
+            prob = 0.003 * (2 ** red_neighbors)
             if random.random() < prob:
                 self.state = 'red'
-                self.red_steps = 1  # Reinicia o contador quando muda para vermelho
+                self.red_steps = 1
 
 class Modelo(Model):
     def __init__(self, width, height):
         self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
 
-        # Preencher a grid com agentes estáticos
         agent_id = 0
         for x in range(width):
             for y in range(height):
-                # Cria um novo agente estático em cada posição (x, y)
                 agent = StaticAgent(agent_id, self, x, y)
                 self.schedule.add(agent)
                 self.grid.place_agent(agent, (x, y))
