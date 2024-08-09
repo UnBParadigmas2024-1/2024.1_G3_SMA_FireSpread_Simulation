9b6d4cd248c6795132626f60072a60773f219b35
valdersonjr <valdersonpontes@gmail.com>
2024-08-09 14:21:41 -0300

    Altera o server.py
    Co-authored-by: pedroblome <joaopedroblome78@gmail.com>

--- server.py
+++ server.py
@@ -2,9 +2,10 @@ from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.UserParam import Slider
 from model import Modelo
+from agent import StaticAgent, DynamicAgent
 
 def agent_portrayal(agent):
-    # Defina as cores baseadas no tipo de vegetação
+    # Define the colors for static agents based on vegetation type
     color_map = {
         'grama': 'lightgreen',
         'arbusto': 'darkgreen',
@@ -12,37 +13,40 @@ def agent_portrayal(agent):
         'terreno_úmido': 'blue'
     }
 
-    # Determine a cor do agente baseado no seu estado atual
-    if agent.state == 'red':
-        color = 'red'
-    elif agent.state == 'gray':
-        color = 'gray'
-    else:
-        color = color_map[agent.vegetation_type]
+    if isinstance(agent, StaticAgent) or isinstance(agent, DynamicAgent):
+        if agent.state == 'red':
+            color = 'red'
+        elif agent.state == 'gray':
+            color = 'gray'
+        elif agent.state == 'active':
+            color = 'black'
+        else:
+            color = color_map[agent.vegetation_type]
+        portrayal = {
+            "Shape": "rect",
+            "Filled": True,
+            "Color": color,
+            "Layer": 0, 
+            "w": 1,
+            "h": 1
+        }
 
-    # Define a aparência do agente na visualização
-    portrayal = {
-        "Shape": "rect",
-        "Filled": True,
-        "Color": color,
-        "Layer": 0,
-        "w": 1,
-        "h": 1
-    }
     return portrayal
 
 
-# Configura a grade de visualizacao
+
+# Configura a grade de visualização
 grid = CanvasGrid(agent_portrayal, 100, 100, 500, 500)
 
-# Parametros do grid, incluindo um slider para ajustar a densidade da floresta
+# Parâmetros do grid, incluindo sliders para ajustar a densidade da floresta e o número de agentes dinâmicos
 grid_params = {
     "width": 100,
     "height": 100,
-    "density": Slider("Densidade", 0.5, 0.1, 1.0, 0.1)
+    "density": Slider("Densidade", 0.5, 0.1, 1.0, 0.1),
+    "num_dynamic_agents": Slider("Número de Agentes Dinâmicos", 1000, 0, 1000, 1)
 }
 
-# Cria o servidor da simulacao, vinculando o modelo e a grade de visualizacao
+# Cria o servidor da simulação, vinculando o modelo e a grade de visualização
 server = ModularServer(Modelo, [grid], "Simulação de Grid com Agente semifixo", grid_params)
 server.port = 8080
 server.launch()
